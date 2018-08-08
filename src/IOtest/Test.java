8c9fe1d29ea863ec8442fef387ea069b1f274572
Hibian <416711267@qq.com>
2018-08-08 20:58:10 +0800

    file io

--- src/IOtest/Test.java
+++ src/IOtest/Test.java
@@ -1,5 +1,31 @@
 package IOtest;
 
-public class Test {
+import java.io.File;
+import java.io.IOException;
 
+public class Test {
+	
+	public static void main(String[] args) throws IOException {
+		File file =  new File("F:\\test\\1.txt");
+		if (!file.exists()) {
+			file.createNewFile();
+			System.out.println("创建成功");
+		}
+//		file.delete();//删除文件
+//		file.isFile();//是否为文件
+//		file.isDirectory();//是否为文件夹
+		
+		File file2 = new File("F:\\test\\my");
+		if (!file2.exists()) {
+			file2.mkdirs();//创建路径
+			System.out.println("ok");
+		}
+//		file.delete();//删除路径需要删除路径下所有文件
+		String[] list = file2.list();//路径下所有文件和文件夹
+		for (String string : list) {
+			System.out.println(string);
+		}
+//		File[] listFiles = file2.listFiles();//返回file[]
+//		listFiles[2].delete();
+	}
 }
--- src/Test04/Test.java
+++ src/Test04/Test.java
@@ -2,22 +2,12 @@ package Test04;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 public class Test {
 
 	public static void main(String[] args) {	
-		Set<Cat> set = new HashSet<>();
-		set.add(new Cat("neko1", 1, 1));
-		set.add(new Cat("neko2", 3, 2));
-		set.add(new Cat("neko3", 3, 3));
-		set.add(new Cat("neko4", 1, 4));
-		set.add(new Cat("neko5", 2, 5));
-		set.add(new Cat("neko5", 2, 5));
-		System.out.println(new Cat("neko5", 2, 5).hashCode()%16);
+		TreeSet<Cat> treeSet = new TreeSet<>();
 		
-		System.out.println(set.size());
-		for (Cat cat : set) {
-			System.out.println(cat);
-		}
 	}
 
 }
